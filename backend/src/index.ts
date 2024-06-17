10fd5bc107800431ae38b4ae27a5159548486bb0
Temka1358 <temulenn@gmail.com>
2024-06-17 13:59:24 +0900

    changes for https test

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,15 +2,34 @@ import app from "./app.js";
 import { connectDatabase } from "./db/connection.js";
 //connection and listeners
 
-const PORT = process.env.PORT ;
+
+// for server
+import https from 'https';
+import fs from 'fs';
+
+const PORT = process.env.PORT || 5000;
+
+// connectDatabase()
+// .then(() => {
+//   app.listen(PORT, () => {
+//     console.log(`Connected to MongoDB and server is listening on port ${PORT}`);
+//   })
+// })
+// .catch((error) => {
+//   console.log("Cannot connect to mongoDB: ", error);
+// });
+
+const options = {
+  key: fs.readFileSync('/var/www/https/server.key'),
+  cert: fs.readFileSync('/var/www/https/server.cert')
+};
 
 connectDatabase()
-.then(() => {
-  app.listen(PORT, () => {
-    console.log(`Connected to MongoDB and server is listening on port ${PORT}`);
+  .then(() => {
+    https.createServer(options, app).listen(PORT, () => {
+      console.log(`Connected to MongoDB and server is listening on port ${PORT}`);
+    });
   })
-})
-.catch((error) => {
-  console.log("Cannot connect to mongoDB: ", error);
-});
-
+  .catch((error) => {
+    console.log("Cannot connect to MongoDB: ", error);
+  });
