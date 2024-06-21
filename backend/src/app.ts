7d042adfac47a2388f5301800e97bf0bf7d2f249
Temka1358 <temulenn@gmail.com>
2024-06-21 11:49:47 +0900

    cors config

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -10,7 +10,7 @@ config();
 const app = express();
 
 //*** Middlewares  *** 
-app.use(cors({origin: 'http://localhost', credentials: true}))
+app.use(cors({origin: 'http://localhost:5173', credentials: true}))
 //it will parse imconing data as json
 app.use(express.json());
 app.use(cookieParser(process.env.COOKIE_SECRET))
